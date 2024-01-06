bb56ba8bdfc152666c157d7bb145608da0221700
Marcelo Rose <mar.rose1999@gmail.com>
2024-01-06 20:10:32 -0300

    added customer events

--- src/domain/@shared/event/event-dispatcher.spec.ts
+++ src/domain/@shared/event/event-dispatcher.spec.ts
@@ -1,6 +1,12 @@
-import SendEmailWhenProductIsCreatedHandler from "../../product/event/handler/send-email-when-product-is-created.handler";
-import ProductCreatedEvent from "../../product/event/product-created.event";
-import EventDispatcher from "./event-dispatcher";
+import CustomerCreatedEvent from '../../customer/event/customer-created.event';
+import SendConsoleLog1WhenCustomerIsCreatedHandler from '../../customer/event/handler/send-console-log-1-when-customer-is-created.handler';
+import SendConsoleLog2WhenCustomerIsCreatedHandler from '../../customer/event/handler/send-console-log-2-when-customer-is-created.handler';
+import SendConsoleLogOnChangeCustomerAddressHandler from '../../customer/event/handler/send-console-log-on-change-customer-address.handler';
+import CustomerFactory from '../../customer/factory/customer.factory';
+import Address from '../../customer/value-object/address';
+import SendEmailWhenProductIsCreatedHandler from '../../product/event/handler/send-email-when-product-is-created.handler';
+import ProductCreatedEvent from '../../product/event/product-created.event';
+import EventDispatcher from './event-dispatcher';
 
 describe("Domain events tests", () => {
   it("should register an event handler", () => {
@@ -76,6 +82,52 @@ describe("Domain events tests", () => {
 
     // Quando o notify for executado o SendEmailWhenProductIsCreatedHandler.handle() deve ser chamado
     eventDispatcher.notify(productCreatedEvent);
+
+    expect(spyEventHandler).toHaveBeenCalled();
+  });
+});
+
+describe("Domain customer events tests", () => {
+  it("should notify when customer is created", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler1 = new SendConsoleLog1WhenCustomerIsCreatedHandler();
+    const eventHandler2 = new SendConsoleLog2WhenCustomerIsCreatedHandler();
+    const spyEventHandler1 = jest.spyOn(eventHandler1, "handle");
+    const spyEventHandler2 = jest.spyOn(eventHandler2, "handle");
+
+    eventDispatcher.register("CustomerCreatedEvent", eventHandler1);
+    eventDispatcher.register("CustomerCreatedEvent", eventHandler2);
+
+    const customerCreatedEvent = new CustomerCreatedEvent({
+      id: 1,
+      name: "Customer 1",
+      address: "Customer 1 address",
+    });
+
+    eventDispatcher.notify(customerCreatedEvent);
+
+    expect(spyEventHandler1).toHaveBeenCalled();
+    expect(spyEventHandler2).toHaveBeenCalled();
+  });
+
+  it("should notify when customer address is changed", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendConsoleLogOnChangeCustomerAddressHandler();
+    const spyEventHandler = jest.spyOn(eventHandler, "handle");
+
+    eventDispatcher.register("CustomerCreatedEvent", eventHandler);
+
+    const customer = CustomerFactory.createWithAddress("Customer 1", new Address("Street", 1, "12345678", "City" ));
+
+    customer.changeAddress(new Address("Street 2", 2, "87654321", "City 2"));
+
+    const customerCreatedEvent = new CustomerCreatedEvent({
+      id: customer.id,
+      name: customer.name,
+      address: customer.Address.toString(),
+    });
+
+    eventDispatcher.notify(customerCreatedEvent);
 
     expect(spyEventHandler).toHaveBeenCalled();
   });
